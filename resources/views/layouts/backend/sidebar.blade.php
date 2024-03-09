6c32c8df7f92aed8d7581c90d03643f2b508fe3b
Ardi Yoto <ardiyoto@gmail.com>
2024-03-09 11:01:29 +0700

    Desain Logo

--- resources/views/layouts/backend/sidebar.blade.php
+++ resources/views/layouts/backend/sidebar.blade.php
@@ -4,7 +4,7 @@
     <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
             <a href="{{ url('/') }}" class="text-nowrap logo-img">
-                <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" width="180" alt="" />
+                <img src="{{ asset('assets/images/logos/SIMRS.png') }}" width="180" alt="" />
             </a>
             <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                 <i class="ti ti-x fs-8"></i>
@@ -14,8 +14,7 @@
         <div class="d-flex align-items-center ms-4 mb-2 mt-4">
             <div class="position-relative">
                 @if (@Auth::user()->foto == null)
-                    <img class="rounded-circle"
-                        src="https://ui-avatars.com/api/?name=Dany&background=007BFF&color=FFF"
+                    <img class="rounded-circle" src="https://ui-avatars.com/api/?name=Dany&background=007BFF&color=FFF"
                         alt="Dany" style="width: 40px; height: 40px;">
                 @else
                     <img class="rounded-circle" src="{{ asset(@Auth::user()->foto) }}" alt="{{ @Auth::user()->name }}"
@@ -56,30 +55,30 @@
                 </li>
                 <li class="nav-small-cap">
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                    <span class="hide-menu">UI COMPONENTS</span>
+                    <span class="hide-menu">Menus</span>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link" href="./ui-buttons.html" aria-expanded="false">
                         <span>
-                            <i class="ti ti-article"></i>
+                            <i class="ti ti-id-badge"></i>
                         </span>
-                        <span class="hide-menu">Buttons</span>
+                        <span class="hide-menu">Dokter</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
                         <span>
-                            <i class="ti ti-alert-circle"></i>
+                            <i class="ti ti-rocket"></i>
                         </span>
-                        <span class="hide-menu">Alerts</span>
+                        <span class="hide-menu">Antrian</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
                         <span>
-                            <i class="ti ti-cards"></i>
+                            <i class="ti ti-files"></i>
                         </span>
-                        <span class="hide-menu">Card</span>
+                        <span class="hide-menu">Rekamedis</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
@@ -118,38 +117,8 @@
                         <span class="hide-menu">Register</span>
                     </a>
                 </li>
-                <li class="nav-small-cap">
-                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                    <span class="hide-menu">EXTRA</span>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="./icon-tabler.html" aria-expanded="false">
-                        <span>
-                            <i class="ti ti-mood-happy"></i>
-                        </span>
-                        <span class="hide-menu">Icons</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="./sample-page.html" aria-expanded="false">
-                        <span>
-                            <i class="ti ti-aperture"></i>
-                        </span>
-                        <span class="hide-menu">Sample Page</span>
-                    </a>
-                </li>
             </ul>
             <div class="unlimited-access hide-menu bg-light-primary position-relative mb-7 mt-5 rounded">
-                <div class="d-flex">
-                    <div class="unlimited-access-title me-3">
-                        <h6 class="fw-semibold fs-4 mb-6 text-dark w-85">Upgrade to pro</h6>
-                        <a href="https://adminmart.com/product/modernize-bootstrap-5-admin-template/" target="_blank"
-                            class="btn btn-primary fs-2 fw-semibold lh-sm">Buy Pro</a>
-                    </div>
-                    <div class="unlimited-access-img">
-                        <img src="../assets/images/backgrounds/rocket.png" alt="" class="img-fluid">
-                    </div>
-                </div>
             </div>
         </nav>
         <!-- End Sidebar navigation -->
